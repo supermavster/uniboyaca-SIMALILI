db4b31a184d5f356b578b5d9c865c7276cfc15e1
Mavsters <mtorres@cittus.com>
2019-04-28 11:07:28 -0500

    [Fix] Image NavBar

--- source/view/template/header.php
+++ source/view/template/header.php
@@ -3,7 +3,7 @@ $body->appendInnerHTML('<header class="header-global">
     <nav id="navbar-main" class="navbar navbar-main navbar-expand-lg navbar-transparent navbar-light headroom">
         <div class="container">
             <a class="navbar-brand mr-lg-5" href="../index/index.html">
-                <img src="' . AS_ASSETS . 'img/logo/logo.png"> SIMALILI
+                <img src="' . AS_ASSETS . 'img/logo/logo-horizontal.png">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar_global"
                     aria-controls="navbar_global" aria-expanded="false" aria-label="Toggle navigation">
