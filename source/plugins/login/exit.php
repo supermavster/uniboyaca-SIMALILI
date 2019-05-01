55236380bef337328ee6c8797f5c147ab51475d7
Mavsters <mtorres@cittus.com>
2019-05-01 09:09:24 -0500

    [Add] Exit

--- source/plugins/login/exit.php
+++ source/plugins/login/exit.php
@@ -8,9 +8,26 @@
  * @copyright  2019 Cittis
  * Copyright (C) 2019 Cittis
  */
+# Remove all Cookies
+function removeCookie($key)
+{
+    if (isset($_COOKIE[$key])) {
+        unset($_COOKIE[$key]);
+        // empty value and old timestamp
+        setcookie($key, '', time() - 3600, '/');
+    }
+}
 
-//Add Library of Control
-require_once '../../../../isv/source/controller/Control.php';
-//Make the index
-$ctrl->finishSession();
+// Remove Cookies
+removeCookie("COOKIE_INDEFINED_SESSION");
+removeCookie("COOKIE_DATA_INDEFINED_SESSION[nombre]");
+removeCookie("COOKIE_DATA_INDEFINED_SESSION[password]");
+// Remove Session
+unset($_SESSION);
+session_unset();
+session_destroy();
+session_start();
+// Return Home
+header("Location: /");
+die();
 ?>
